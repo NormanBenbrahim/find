62045ab5d63c7b15d26e5b4466c1c72d7f902b68
NormanBenbrahim <norman.benbrahim@gmail.com>
2020-04-28 19:19:42 -0400

    complete

---
+++ script.py
@@ -0,0 +1,11 @@
+my_message = "I like YouTube because there are people who like programming that come and watch videos with me! :) $$ 94454 😶"
+
+#print(my_message.find('$'))
+
+#print(my_message[100])
+
+
+if my_message.find(':)', 3, 59) != -1:
+    print("I'm rich!")
+else:
+    print("Oh nooooo")
